0079fd3d028d846a8919cc4827d816d5a68efaf6
sudhir kumar <Sudhirkmr40@gmail.com>
2022-08-04 07:17:14 +0530

    initial commit

--- json files.py
+++ json files.py
@@ -1,5 +1,11 @@
 import pandas as pd
+
+
 df= pd.read_excel('E:/Study\python/24-july/data fsds_/Attribute DataSet.xlsx')
 
-
 Json = df.to_json('test1234.json')
+df.to_json('E:/Study/python/24-july/test123.json')
+
+df5 = pd.read_json('E:/Study/python/24-july/test123.json')
+print(df5)
+
---
+++ sales data.py
@@ -0,0 +1,7 @@
+import  pandas as pd
+
+data = pd.read_csv('E:/Study/python/30 july/sales_data_final.csv')
+print(data)
+#
+data.head()
+data.tail()
